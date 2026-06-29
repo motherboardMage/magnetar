2ff5eea9e4499ac45025bd1959c5fb7466c8cb3a
Nishchay Mishra <nishchaymishra4151@gmail.com>
2026-06-29 11:05:19 +0530

    fix: return Error to stop list/dictionary parsing on b'e'

--- src/torrent_file/skip_values.rs
+++ src/torrent_file/skip_values.rs
@@ -1,7 +1,9 @@
 use nom::bytes::complete::{tag, take};
 use nom::character::complete::{digit1, u64 as nom_u64};
 use nom::combinator::{opt, peek, recognize};
+use nom::multi::many0_count;
 use nom::number::complete::u8 as nom_u8;
+use nom::sequence::delimited;
 use nom::{IResult, Parser};
 
 pub fn skip_bencode_value(input: &[u8]) -> IResult<&[u8], ()> {
@@ -12,6 +14,12 @@ pub fn skip_bencode_value(input: &[u8]) -> IResult<&[u8], ()> {
         b'0'..=b'9' => skip_string(input),
         b'l' => skip_list(input),
         b'd' => skip_dictionary(input),
+        b'e' => {
+            return Err(nom::Err::Error(nom::error::Error::new(
+                input,
+                nom::error::ErrorKind::Tag,
+            )));
+        }
         _ => {
             return Err(nom::Err::Failure(nom::error::Error::new(
                 input,
