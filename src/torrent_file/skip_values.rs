de07852872367dfb15ac8d364a7667e712e85c04
Nishchay Mishra <nishchaymishra4151@gmail.com>
2026-07-04 19:29:07 +0530

    fix: skip_dictionary now ensures valid key type

--- src/torrent_file/skip_values.rs
+++ src/torrent_file/skip_values.rs
@@ -55,7 +55,7 @@ fn skip_list(input: &[u8]) -> IResult<&[u8], ()> {
 fn skip_dictionary(input: &[u8]) -> IResult<&[u8], ()> {
     let (remaining, _) = delimited(
         tag("d"),
-        many0_count((skip_bencode_value, skip_bencode_value)),
+        many0_count((skip_string, skip_bencode_value)),
         tag("e"),
     )
     .parse(input)?;
