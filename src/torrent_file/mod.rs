56b057665d2b7067f88aafb60cefa3092b9b3393
Nishchay Mishra <nishchaymishra4151@gmail.com>
2026-08-02 19:17:19 +0530

    refactor: replace owned String fields with borrowed byte slices
    Switch announce, announce_list, comment, created_by, and name from
    String to &'a [u8] to enable zero-copy parsing from the .torrent
    file buffer.

--- src/torrent_file/mod.rs
+++ src/torrent_file/mod.rs
@@ -5,14 +5,14 @@ pub mod skip_values;
 
 #[derive(Debug, Clone)]
 pub struct Torrent<'a> {
-    pub announce: String,
+    pub announce: &'a [u8],
     pub info: Info<'a>,
     pub piece_layers: Option<BTreeMap<&'a [u8], &'a [u8]>>,
 
     /// Optional fields
-    pub announce_list: Option<Vec<Vec<String>>>,
-    pub comment: Option<String>,
-    pub created_by: Option<String>,
+    pub announce_list: Option<Vec<Vec<&'a [u8]>>>,
+    pub comment: Option<&'a [u8]>,
+    pub created_by: Option<&'a [u8]>,
     pub creation_date: Option<i64>,
 
     // Raw info bytes for hashing
@@ -21,7 +21,7 @@ pub struct Torrent<'a> {
 
 #[derive(Debug, Clone)]
 pub struct Info<'a> {
-    pub name: String,
+    pub name: &'a [u8],
     pub piece_length: usize,
     pub meta_version: Option<i64>,
     pub file_layout: FileLayout<'a>,
